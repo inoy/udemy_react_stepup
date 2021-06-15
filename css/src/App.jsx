a8fd65ab1a31ac43925001862ffe1c2f83ef797a
inoy <7m7110123@gmail.com>
2021-06-15 21:49:06 +0900

    様々なCSSのあてかたに触れる

---
+++ css/src/App.jsx
@@ -0,0 +1,12 @@
+import './styles.css';
+import { InlineStyle } from './InlineStyle';
+
+const App = () => {
+  return (
+    <div className='App'>
+      <InlineStyle />
+    </div>
+  );
+};
+
+export default App;
